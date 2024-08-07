3e87dbf2644941c744eadb6a17153916addc4f40
Angela Yepes <angelyep76@gmail.com>
2024-08-07 14:17:12 -0500

    metodo para ordenar el arreglo

--- src/TallerArreglos.java
+++ src/TallerArreglos.java
@@ -66,6 +66,15 @@ public class TallerArreglos {
 
     // Método para ordenar el arreglo (puedes usar el algoritmo de burbuja)
     public static void ordenarArreglo(int[] arr) {
-        // Implementa este método
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
